f78e7c5a85ca29dd3fff84014c6f3b0de57fe019
Sebastian Chavarria <sebastianchavarria570@gmail.com>
2026-07-03 11:30:03 -0600

    Agreugue la clase PlataformaStreaming

---
+++ src/PlataformaStreaming.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class PlataformaStreaming {
+    private ArrayList<CuentaUsuario> cuentas;
+
+    public PlataformaStreaming() {
+        this.cuentas = new ArrayList<>();
+    }
+
+    public void AgregarCuentaUsuario(CuentaUsuario cuenta){
+        this.cuentas.add(cuenta);
+    }
+
+
+
+
+
+
+
+
+}
