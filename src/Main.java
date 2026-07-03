26ff79f76e3d507ef642be6e35fcbaeeeea1d07a
Lalo4r <gerapm120507@gmail.com>
2026-07-03 11:12:42 -0600

    Agregue parte del menu de main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
 
+        int opcion = 0;
+
+//        plataformaStreaming = new plataformaStreaming();
+
+        do {
+            try {
+                System.out.println("---------------Plataforma de streaming----------");
+                System.out.println("1.Agregar usuario");
+                System.out.println("Elige una opcion: ");
+                opcion = teclado.nextInt();
+
+
+            }catch (Exception e){
+                System.out.println("Error, intenta de nuevo");
+                teclado.nextLine();
+            }
+        }while (opcion != 8);
     }
 }
